2082b765ac7d57958e3ee5c1626eb0812487bd97
Dominik Piotrowicz <dom.piotrowicz@gmail.com>
2022-11-16 02:27:07 +0100

    remove cached

--- api/dist/utils/timeToMidnight.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getTimeToMidnight(date) {
-    const midnight = new Date(date);
-    midnight.setHours(24, 0, 0, 0);
-    return midnight.getTime() - date.getTime();
-}
-exports.default = getTimeToMidnight;
